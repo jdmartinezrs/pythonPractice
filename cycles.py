6f43259bb6edf50427aff2b1d78d8df913687578
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2024-11-09 23:40:15 -0500

    feat: :sparkles: At this stage, I’ve been revisiting cycle handling in Python

---
+++ cycles.py
@@ -0,0 +1,95 @@
+# cycles, iterations, loop's
+
+#While: it executes and keeps iterating until the result is false
+"""
+i=0
+while i < 10:
+    if i < 5:
+        print("The number", i, "Is less than 5")
+    else:
+        print("The number", i , "Is equal or greater than 5")
+
+    i += 1
+
+print('The iteration has finished')
+
+"""
+
+#For cycle to interate in ranges
+"""
+for i in "davidR10":
+    print(i)
+"""
+
+"""
+for i in range(10):
+    print(i)
+"""
+
+"""
+for i in "jorge":
+    print(i)
+    """
+"""
+for i in range (0,100,50):
+    print("glaera",i,"o num e")
+"""
+
+"""
+print("Guess the name!")
+name = input("Enter your guess: ")
+
+while name != "davidR10":
+    print("Incorrect. Try again.")
+    name = input("Enter your guess: ")
+
+print("Congratulations! You guessed the name correctly.")
+print("Here are the letters in the name:")
+
+for letter in name:
+    print(letter)
+
+
+
+print('Guess the number')
+"""
+
+
+"""
+number = int(input("Enter a number from 1 to 15: "))
+
+while number != 10:
+    if 1 <= number < 5:
+        print("You are far away, try a higher number.")
+    elif 5 <= number < 8:
+        print("You are getting closer, try a higher number.")
+    elif 8 <= number < 10:
+        print("You are very close, just a bit higher!")
+    elif 10 < number <= 13:
+        print("You are very close, try a slightly lower number.")
+    elif 13 < number <= 15:
+        print("You are close, but try a smaller number.")
+    else:
+        print("The number is out of range. Enter a number between 1 and 15.")
+        
+    number = int(input("Enter a number: "))
+
+print("Congratulations! You have guessed the number.")
+"""
+
+"""
+for letter in "davidR10":
+    print(letter)
+
+for word in "marthaRocio":
+    print(word)
+"""
+"""
+for i in range (2,50,3):
+    print("number",i)
+"""
+
+
+for i in range (20):
+    print(i)
+
--- tienda.py
+++ tienda.py
@@ -10,16 +10,38 @@ total_animals = num_dogs + num_cats + num_birds
 
 print('Please insert your name')
 name = input()
-print ("Por favor escribe tu apellido")
+print ("Please insert your lastname")
 last_name = input()
 
 #Concatenation
 full_name = name + " " + last_name
 
 print('thanks for visit us', full_name)
-print ("Currently we have:")
-print ( num_dogs, "dogs", num_cats, "cats", num_birds,"birds")
-print ("We have in total ",total_animals,"animals")
+
+print("Select the option you prefer")
+print("1: To know how many animals we have")
+print("2. To buy a animal")
+
+respuesta = int(input())
+
+if respuesta == 1:
+    print ("Currently we have:")
+    print ( num_dogs, "dogs", num_cats, "cats", num_birds,"birds")
+    print ("We have in total ",total_animals,"animals")
+elif respuesta == 2:
+    print("Wich animal do you want to buy")
+    animal = input()
+    article = "an" if animal[0].lower() in "aeiou" else "a"
+    print(f"You have bought {article} {animal}")
+    
+
+
+
+
+
+
+
+
 
 
 
